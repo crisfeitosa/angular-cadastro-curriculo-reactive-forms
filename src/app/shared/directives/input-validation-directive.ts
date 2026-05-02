8bb99c9909a3fb5c8c508c8ff96348a30dc4b33e
Cristiano Feitosa <cris.immobilis@gmail.com>
2026-05-02 14:00:09 -0300

    feat: refactor input validation directive to improve styling management on validation status

--- src/app/shared/directives/input-validation-directive.ts
+++ src/app/shared/directives/input-validation-directive.ts
@@ -1,4 +1,12 @@
-import { DestroyRef, Directive, HostListener, inject, Input } from '@angular/core';
+import {
+  DestroyRef,
+  Directive,
+  ElementRef,
+  HostListener,
+  inject,
+  Input,
+  Renderer2,
+} from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { NgControl } from '@angular/forms';
 
@@ -9,6 +17,8 @@ import { NgControl } from '@angular/forms';
 export class InputValidationDirective {
   private readonly _ngControl = inject(NgControl, { self: true });
   private readonly _destroyRef = inject(DestroyRef);
+  private readonly _elementRef = inject(ElementRef);
+  private readonly _renderer2 = inject(Renderer2);
 
   @Input() errorMessage = 'Campo inválido';
 
@@ -33,9 +43,35 @@ export class InputValidationDirective {
     const isInvalid = control.invalid && control.touched;
 
     if (isInvalid) {
+      this.setInvalidStyles();
       console.log('Está inválido:', this.errorMessage);
     } else {
+      this.setValidStyles();
       console.log('Está válido');
     }
+  }
+
+  private setInvalidStyles() {
+    const el = this._elementRef.nativeElement;
+
+    this._renderer2.removeClass(el, 'border-zinc-800');
+    this._renderer2.removeClass(el, 'focus:border-violet-500');
+    this._renderer2.removeClass(el, 'focus:ring-violet-500');
+
+    this._renderer2.addClass(el, 'border-red-500');
+    this._renderer2.addClass(el, 'focus:border-red-500');
+    this._renderer2.addClass(el, 'focus:ring-red-500');
+  }
+
+  private setValidStyles() {
+    const el = this._elementRef.nativeElement;
+
+    this._renderer2.removeClass(el, 'border-red-500');
+    this._renderer2.removeClass(el, 'focus:border-red-500');
+    this._renderer2.removeClass(el, 'focus:ring-red-500');
+
+    this._renderer2.addClass(el, 'border-zinc-800');
+    this._renderer2.addClass(el, 'focus:border-violet-500');
+    this._renderer2.addClass(el, 'focus:ring-violet-500');
   }
 }
